de71f0b87bbbfb68a514c40ccd1c609171649e7f
Vamerlen <kongolovafadie@gmail.com>
2024-12-12 13:47:53 +0200

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,82 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+  },
+  {
+    id: 4,
+    emoji: "🥓",
+    name: "Bacon",
+    meaning:
+      "I must save my bacon! Crispy fried bacon strips. Component of many hearty dishes, such as burgers or sandwiches. Together with eggs, part of a traditional American breakfast.",
+  },
+  {
+    id: 5,
+    emoji: "🍜",
+    name: "Ramen",
+    meaning:
+      "Japanese dish with ramen noodles. Traditionally served with chopsticks. The steaming bowl can also represent a warm, cooked meal such as soup.",
+  },
+  {
+    id: 1,
+    emoji: "🍭",
+    name: "Lolipop",
+    meaning:
+      "You're so sweet! or “I want candy!“ The lollipop conveys fun and reminds us of our childhood.Popular emoji of food bloggers.",
+  },
+  {
+    id: 2,
+    emoji: "🍯",
+    name: "Honey Pot",
+    meaning:
+      "“The land of milk and honey“ or “You catch more flies with honey than with vinegar“. Made by bees, desired by bears. Emoji also stands for: afacility within computer networks to watch hackers, a swimming device for Pooh Bear as well as place of setting in Harry Potter novels",
+  },
+
+  {
+    id: 3,
+    emoji: "🥧",
+    name: "Pie",
+    meaning:
+      "I love cake or “American Pie is my favorite movie“! Ingredients stewed under a cover of dough, savory or sweet. Crispy outside, juicy inside: Pies are popular in the English and American areas",
+  },
+  {
+    id: 1,
+    emoji: "🍩",
+    name: "Donut",
+    meaning:
+      "You crave for something sweet! Policemen are constantly eating doughnuts in American movies, and they are also known to be Homer Simpson's favorite food",
+  },
+  {
+    id: 2,
+    emoji: "🍫",
+    name: "Chocolate",
+    meaning:
+    "“I love chocolate“ or “chocolate makes you happy“! An emoji for all chocolate lovers. Main ingredients are cocoa products from the cocoa tree as well as sugar. The most chocolate is eaten in Europe and North America.",
+},
+];
+
+export default emojipedia;
